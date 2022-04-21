cb076c84758b71b5338b0764e81102bcc4c063f5
Igor Lunin <postrel19@yandex.ru>
2022-04-21 17:40:32 +0300

    Q

--- ParallelStages.groovy
+++ ParallelStages.groovy
@@ -1,60 +1,52 @@
 pipeline {
-
-    agent none
-
-    parameters{
-        booleanParam(name: 'ALFA', defaultValue: true, description: 'Build ALFA')
-        booleanParam(name: 'BETA', defaultValue: true, description: 'Build BETA')
-        booleanParam(name: 'ZETA', defaultValue: false, description: 'Build ZETA')
-
-        booleanParam(name: 'All', defaultValue: true, description: 'Build All Platforms')
-        booleanParam(name: 'Win64', defaultValue: false, description: 'Build Win64')
-        booleanParam(name: 'PS4', defaultValue: false, description: 'Build PS4')
-        booleanParam(name: 'PS5', defaultValue: false, description: 'Build PS5')
-        booleanParam(name: 'XSX', defaultValue: false, description: 'Build XSX')
-        booleanParam(name: 'XBoxOneGDK', defaultValue: false, description: 'Build XBoxOneGDK')
-        booleanParam(name: 'Server', defaultValue: false, description: 'Build Server')
+    agent any
+    options {
+        parallelsAlwaysFailFast()
     }
     stages {
-        stage('Run builds') {
+        stage('Non-Parallel Stage') {
             steps {
-            PlatformsForBuild = ['PS4','XSX','PS5','XBoxOneGDK','Win64','Server']
-            AllAgents = ['ALFA','BETA','ZETA']
-            Platforms =[:]
-            Agents = [:]
-            for (p in PlatformsForBuild){
-                if (params.p){
-                    Platforms.p = true
-                }
+                echo 'This stage will be executed first.'
             }
-            for (n in AllAgents){
-                if (params.n){
-                    Agents.n = true
-                }
+        }
+        stage('Parallel Stage') {
+            when {
+                branch 'master'
             }
-            for (p in PlatformsForBuild){
-                if (Platforms.p){
-                    for (n in AllAgents){
-                        if (Agents.n){
-                            parallel {
-                                Agents.n=false
-                                    stage("${PlatformsForBuild.p} in ${AllAgents.n}") {
-                                        agent {
-                                            docker {image 'docker/getting-started '}
-                                        }
-                                        steps {
-                                            echo p+"${PlatformsForBuild.p}"+n+"${AllAgents.n}"
-                                            Platforms.p=false
-                                            sh 'node --version'
-                                        }
-                                    }
-                                Agents.n=true
-                            }
-                        }
-                    break
+            parallel {
+                stage('Branch A') {
+                    agent {
+                        label "for-branch-a"
+                    }
+                    steps {
+                        echo "On Branch A"
                     }
                 }
-            }
+                stage('Branch B') {
+                    agent {
+                        label "for-branch-b"
+                    }
+                    steps {
+                        echo "On Branch B"
+                    }
+                }
+                stage('Branch C') {
+                    agent {
+                        label "for-branch-c"
+                    }
+                    stages {
+                        stage('Nested 1') {
+                            steps {
+                                echo "In stage Nested 1 within Branch C"
+                            }
+                        }
+                        stage('Nested 2') {
+                            steps {
+                                echo "In stage Nested 2 within Branch C"
+                            }
+                        }
+                    }
+                }
             }
         }
     }
