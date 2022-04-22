3ebc823ce8cd5ef4b3ab0893f80c397076f7406a
Igor Lunin <postrel19@yandex.ru>
2022-04-22 17:37:58 +0300

    -env

--- ParallelStages.groovy
+++ ParallelStages.groovy
@@ -1,10 +1,10 @@
-def env.MassInMass = ['a','b','c','d','e','f']
-def env.Massive = [a:'true',b:'true',c:'true',d:'true',e:'true',f:'true']
+def MassInMass = ['a','b','c','d','e','f']
+def Massive = [a:'true',b:'true',c:'true',d:'true',e:'true',f:'true']
 def foo1 = {m, mm ->
     for (p in mm){
-        if (env.Massive.p){
+        if (Massive.p){
             echo p
-            env.Massive.p = false
+            Massive.p = false
             echo p
             sleep(10)
         }
@@ -12,9 +12,9 @@ def foo1 = {m, mm ->
 }
 def foo2 = {m,mm ->
     for (p in mm){
-        if (env.MassInMass.p){
+        if (MassInMass.p){
             echo p
-            env.Massive.p = false
+            Massive.p = false
             echo p
             sleep(10)
 
@@ -29,8 +29,8 @@ pipeline {
     stages {
         stage('Non-Parallel Stage') {
             steps {
-                echo env.MassInMas
-                echo env.Massive
+                echo MassInMas
+                echo Massive
             }
         }
         stage('Parallel Stage') {
@@ -47,7 +47,7 @@ pipeline {
                     // }
                     steps {
                         script{
-                        foo1(env.Massive, env.MassInMass)
+                        foo1(Massive, MassInMass)
                         }
                     }
                 }
@@ -63,7 +63,7 @@ pipeline {
                     // }
                     steps {
                         script{
-                        foo2 (env.Massive, env.MassInMass)
+                        foo2 (Massive, MassInMass)
                         }
                     }
                         
