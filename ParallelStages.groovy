59532268cb20f5f5d9f8f270d7717e4d40584b1f
Igor Lunin <postrel19@yandex.ru>
2022-04-25 11:15:31 +0300

    step -scrip

--- ParallelStages.groovy
+++ ParallelStages.groovy
@@ -20,14 +20,17 @@ pipeline {
     stages {
         stage('Non-Parallel Stage') {
             steps {
-
-
+                echo env.MassInMass
+                echo env.Massive
+                
             }
         }
         stage('Parallel Stage') {
-            script {
-                def Massive = ['a':'true','b':'true','c':'true','d':'true','e':'true','f':'true']
-                def MassInMass = ['a','b','c','d','e','f'] as String[]
+            step{
+                script{
+                    def Massive = ['a':'true','b':'true','c':'true','d':'true','e':'true','f':'true']
+                    def MassInMass = ['a','b','c','d','e','f'] as String[]
+                }
             }
             parallel {
                 stage('Branch A') {
